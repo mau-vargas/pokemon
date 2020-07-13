8a67f4c1c8dae1c15feed4bca139e0afa0a15191
mvargas <mvargas@technisys.com>
2020-07-13 19:15:44 -0400

    se agrega dependencia de dagger

---
+++ app/src/main/java/com/android/pokemon/presentation/MainApplication.kt
@@ -0,0 +1,16 @@
+package com.android.pokemon.presentation
+
+import android.app.Application
+
+class MainApplication : Application() {
+
+    companion object {
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
+
