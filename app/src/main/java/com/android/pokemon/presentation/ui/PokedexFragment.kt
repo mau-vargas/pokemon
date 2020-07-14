e30de6be91167da0f69f00c3f717169c6244a350
mvargas <mvargas@technisys.com>
2020-07-13 23:40:06 -0400

    se agrega buscador

--- app/src/main/java/com/android/pokemon/presentation/ui/PokedexFragment.kt
+++ app/src/main/java/com/android/pokemon/presentation/ui/PokedexFragment.kt
@@ -1,7 +1,13 @@
 package com.android.pokemon.presentation.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.viewModels
@@ -14,6 +20,7 @@ import com.android.pokemon.presentation.appComponent
 import com.android.pokemon.presentation.util.BaseFragment
 import com.android.pokemon.presentation.util.Failure
 import com.android.pokemon.presentation.util.look
+import kotlinx.android.synthetic.main.fragment_pokedex.view.*
 import javax.inject.Inject
 
 class PokedexFragment : BaseFragment() {
@@ -24,7 +31,6 @@ class PokedexFragment : BaseFragment() {
 
     private val viewModel: PokedexViewModel by viewModels { viewModelFactory }
 
-    private var swipeRefresh: SwipeRefreshLayout? = null
 
     @Inject
     lateinit var pokedexAdapter: PokedexAdapter
@@ -34,8 +40,28 @@ class PokedexFragment : BaseFragment() {
         binding = DataBindingUtil.bind<ViewDataBinding>(view) as FragmentPokedexBinding
         appComponent().inject(this@PokedexFragment)
 
-
+        setUpToolBar(
+            binding.toolbar,
+            R.string.app_name,
+            R.drawable.ic_pokeball
+        )
         initLiveData()
+    }
+
+    protected fun setUpToolBar(
+        toolbar: Toolbar,
+        toolbarTitle: Int,
+        resId: Int = R.drawable.ic_launcher_background
+    ) {
+        setHasOptionsMenu(true)
+        with(activity as AppCompatActivity) {
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.title = ""
+            supportActionBar?.setHomeAsUpIndicator(resId)
+            toolbar.toolbar_title.text = getString(toolbarTitle)
+            supportActionBar?.show()
+        }
     }
 
     private fun initLiveData() {
@@ -81,6 +107,40 @@ class PokedexFragment : BaseFragment() {
         lim.orientation = LinearLayoutManager.VERTICAL
         binding.recyclerViewItem.layoutManager = lim
         binding.recyclerViewItem.adapter = pokedexAdapter
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_item_menu, menu)
+        val searchItem = menu.findItem(R.id.new_search)
+        val searchView = searchItem.actionView as SearchView
+
+        val searchAutoComplete =
+            searchView.findViewById<View>(R.id.search_src_text) as SearchView.SearchAutoComplete
+        searchAutoComplete.setHintTextColor(resources.getColor(R.color.colorPrimary))
+        searchAutoComplete.setTextColor(resources.getColor(R.color.colorPrimary))
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(s: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(textoFiltrado: String): Boolean {
+                pokedexAdapter.filter.filter(textoFiltrado)
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                requireActivity().onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun editItem(id: Long) {
