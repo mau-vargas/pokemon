1dc988aa6ad0dff8e8686e0b3ad18fbf9ef06964
mvargas <mvargas@technisys.com>
2020-07-17 17:17:53 -0400

    se eliminan entidades que no se ocupan

--- app/src/main/java/com/android/pokemon/domain/entity/evolution/EvolvesTo.kt
+++ app/src/main/java/com/android/pokemon/domain/entity/evolution/EvolvesTo.kt
@@ -1,7 +1,6 @@
 package com.android.pokemon.domain.entity.evolution
 
 data class EvolvesTo(
-    val evolution_details: List<EvolutionDetail>,
     val evolves_to: List<EvolvesToX>,
     val is_baby: Boolean,
     val species: SpeciesX
--- app/src/main/java/com/android/pokemon/domain/entity/evolution/EvolvesToX.kt
+++ app/src/main/java/com/android/pokemon/domain/entity/evolution/EvolvesToX.kt
@@ -1,7 +1,6 @@
 package com.android.pokemon.domain.entity.evolution
 
 data class EvolvesToX(
-    val evolution_details: List<EvolutionDetailX>,
     val evolves_to: List<Any>,
     val is_baby: Boolean,
     val species: Species
